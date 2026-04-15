33c6586d3da47a95c9a6fbf092fad7f429afb5dd
g <165684225+devkimze@users.noreply.github.com>
2026-04-15 22:34:12 +0900

    upload

--- register.js
+++ register.js
@@ -5,6 +5,14 @@ dotenv.config();
 const commands = [
   new SlashCommandBuilder().setName("download").setDescription("다운로드"),
   new SlashCommandBuilder().setName("dl").setDescription("다운로드")
+  new SlashCommandBuilder()
+  .setName("upload")
+  .setDescription("ini 업로드")
+  .addAttachmentOption(option =>
+    option.setName("file")
+      .setDescription("ini 파일")
+      .setRequired(true)
+  )
 ].map(cmd => cmd.toJSON());
 
 const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
