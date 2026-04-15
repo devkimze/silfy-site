741c93909d15faab3f7cf211bcebaa8533c1f1e8
g <165684225+devkimze@users.noreply.github.com>
2026-04-16 03:13:44 +0900

    Fix indentation in register.js

--- register.js
+++ register.js
@@ -72,6 +72,7 @@ const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
 
 await rest.put(
   Routes.applicationGuildCommands(
+  process.env.CLIENT_ID,
   process.env.GUILD_ID
 ),
   { body: commands }
