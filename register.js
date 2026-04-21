643666a2fe6325a4f400ca13a42006ec8ad21f55
g <165684225+devkimze@users.noreply.github.com>
2026-04-21 12:15:45 +0900

    Update register.js

--- register.js
+++ register.js
@@ -3,93 +3,48 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const commands = [
+
+  // =====================
+  // download
+  // =====================
   new SlashCommandBuilder()
     .setName("download")
     .setDescription("다운로드"),
 
+  // =====================
+  // dl (별칭)
+  // =====================
   new SlashCommandBuilder()
     .setName("dl")
     .setDescription("다운로드"),
 
+  // =====================
+  // nip 업로드
+  // =====================
   new SlashCommandBuilder()
-    .setName("upload")
-    .setDescription("ini 업로드")
+    .setName("nip")
+    .setDescription("nip 업로드")
     .addStringOption(o =>
       o.setName("name")
-        .setDescription("config 이름") 
+        .setDescription("파일 이름")
         .setRequired(true)
     )
     .addAttachmentOption(o =>
       o.setName("file")
-        .setDescription("ini 파일") 
-        .setRequired(true)
-    ),
-
-  new SlashCommandBuilder()
-    .setName("update")
-    .setDescription("ini 업데이트")
-    .addStringOption(o =>
-      o.setName("name")
-        .setDescription("config 이름")
+        .setDescription("nip 파일")
         .setRequired(true)
     )
-    .addAttachmentOption(o =>
-      o.setName("file")
-        .setDescription("ini 파일")
-        .setRequired(true)
-    ),
 
-  new SlashCommandBuilder()
-  .setName("nip")
-  .setDescription("nip 업로드")
-  .addStringOption(o =>
-    o.setName("name")
-      .setDescription("파일 이름")
-      .setRequired(true)
-  )
-  .addAttachmentOption(o =>
-    o.setName("file")
-      .setDescription("nip 파일")
-      .setRequired(true)
-  ),
-  
-  new SlashCommandBuilder()
-  .setName("suser")
-  .setDescription("유저 관리")
-  .addSubcommand(sub =>
-    sub.setName("add")
-      .setDescription("등록")
-      .addStringOption(o =>
-        o.setName("name").setDescription("닉네임").setRequired(true))
-      .addStringOption(o =>
-        o.setName("id").setDescription("발로 ID").setRequired(true))
-  )
-  .addSubcommand(sub =>
-    sub.setName("get")
-      .setDescription("조회")
-      .addStringOption(o =>
-        o.setName("name").setDescription("닉네임").setRequired(true))
-  )
-  .addSubcommand(sub =>
-    sub.setName("delete")
-      .setDescription("삭제")
-      .addStringOption(o =>
-        o.setName("name").setDescription("닉네임").setRequired(true))
-  )
-  .addSubcommand(sub =>
-  sub.setName("list")
-    .setDescription("목록")
-)
 ].map(cmd => cmd.toJSON());
 
 const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
 
 await rest.put(
   Routes.applicationGuildCommands(
-  process.env.CLIENT_ID,
-  process.env.GUILD_ID
-),
+    process.env.CLIENT_ID,
+    process.env.GUILD_ID
+  ),
   { body: commands }
 );
 
-console.log("등록 완료");
+console.log("등록 완료 (download / dl / nip만)");
